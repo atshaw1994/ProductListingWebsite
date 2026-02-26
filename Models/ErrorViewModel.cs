fecbe42686d20775c657c0cce2cbec5e5f5e979b
atshaw1994 <atshaw1994@gmail.com>
2026-02-25 19:58:21 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductListingWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
